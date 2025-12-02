1cfcac7b17ade64ef0e882ba0887dea17dd2fbe1
BregNights <daniel_baumann567@hotmail.com>
2025-12-02 18:30:35 -0300

    add authenticate

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,14 +1,24 @@
+import { AuthenticateCourierUseCase } from "@/domain/carrier/application/use-cases/authenticate-courier"
 import { RegisterCourierUseCase } from "@/domain/carrier/application/use-cases/register-courier"
 import { RegisterRecipientUseCase } from "@/domain/carrier/application/use-cases/register-recipient"
 import { Module } from "@nestjs/common"
 import { CryptographyModule } from "../cryptography/cryptography.module"
+import { AuthenticateController } from "./controllers/authenticate-controller"
 import { CreateAccountController } from "./controllers/create-account.controller"
 import { CreateRecipientController } from "./controllers/create-recipient.controller"
 import { DatabaseModule } from "./database/database.module"
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
-  controllers: [CreateAccountController, CreateRecipientController],
-  providers: [RegisterCourierUseCase, RegisterRecipientUseCase],
+  controllers: [
+    CreateAccountController,
+    CreateRecipientController,
+    AuthenticateController,
+  ],
+  providers: [
+    RegisterCourierUseCase,
+    RegisterRecipientUseCase,
+    AuthenticateCourierUseCase,
+  ],
 })
 export class HttpModule {}
