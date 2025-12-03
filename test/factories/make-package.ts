f7f3b3e7132942bf4432c29d5e0ffd42fb482676
BregNights <daniel_baumann567@hotmail.com>
2025-12-03 20:40:40 -0300

    update

--- test/factories/make-package.ts
+++ test/factories/make-package.ts
@@ -3,7 +3,10 @@ import {
   Package,
   PackageProps,
 } from "@/domain/carrier/enterprise/entities/package"
+import { PrismaPackageMapper } from "@/infra/database/prisma/mappers/prisma-package-mapper"
+import { PrismaService } from "@/infra/database/prisma/prisma.service"
 import { faker } from "@faker-js/faker"
+import { Injectable } from "@nestjs/common"
 
 export function makePackage(
   override: Partial<PackageProps> = {},
@@ -20,4 +23,19 @@ export function makePackage(
   )
 
   return pkg
+}
+
+@Injectable()
+export class PackageFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaPackage(data: Partial<PackageProps> = {}): Promise<Package> {
+    const pkg = makePackage(data)
+
+    await this.prisma.package.create({
+      data: PrismaPackageMapper.toPrisma(pkg),
+    })
+
+    return pkg
+  }
 }
