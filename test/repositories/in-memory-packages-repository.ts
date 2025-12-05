770483c36130764a63d33166dcf5ad529d33815c
BregNights <daniel_baumann567@hotmail.com>
2025-12-05 16:44:54 -0300

    findManyPackagesByCourierId return PackageDetails

--- test/repositories/in-memory-packages-repository.ts
+++ test/repositories/in-memory-packages-repository.ts
@@ -2,10 +2,14 @@ import { DomainEvents } from "@/core/events/domain-events"
 import { PackagesRepository } from "@/domain/carrier/application/repositories/packages-repository"
 import { Package } from "@/domain/carrier/enterprise/entities/package"
 import { PackageDetails } from "@/domain/carrier/enterprise/entities/value-objects/package-details"
+import { InMemoryCouriersRepository } from "./in-memory-couriers-repository"
 import { InMemoryRecipientsRepository } from "./in-memory-recipients-repository"
 
 export class InMemoryPackagesRepository implements PackagesRepository {
-  constructor(private recipientsRepository: InMemoryRecipientsRepository) {}
+  constructor(
+    private recipientsRepository: InMemoryRecipientsRepository,
+    private couriersRepository: InMemoryCouriersRepository
+  ) {}
 
   public items: Package[] = []
 
@@ -63,10 +67,46 @@ export class InMemoryPackagesRepository implements PackagesRepository {
   async findManyPackagesByCourierId(
     courierId: string,
     page: number
-  ): Promise<Package[]> {
+  ): Promise<PackageDetails[]> {
     const pkgs = this.items
       .filter((item) => item.courierId?.toString() === courierId)
       .slice((page - 1) * 20, page * 20)
+      .map((pkg) => {
+        const recipient = this.recipientsRepository.items.find((recipient) =>
+          recipient.id.equals(pkg.recipientId)
+        )
+
+        if (!recipient) {
+          throw new Error(
+            `Recipient with ID "${pkg.recipientId.toString()}" does not exist.`
+          )
+        }
+
+        const courier = this.couriersRepository.items.find((courier) =>
+          courier.id.equals(pkg.courierId!)
+        )
+
+        if (!courier) {
+          throw new Error(
+            `Courier with ID "${pkg.courierId?.toString()}" does not exist.`
+          )
+        }
+
+        return PackageDetails.create({
+          packageId: pkg.id,
+          trackingCode: pkg.trackingCode,
+          description: pkg.description,
+          status: pkg.status,
+
+          recipientId: recipient.id,
+          recipientAddress: recipient.address,
+          recipientName: recipient.name,
+          recipientPhone: recipient.phone,
+
+          createdAt: pkg.createdAt,
+          updatedAt: pkg.updatedAt,
+        })
+      })
 
     return pkgs
   }
