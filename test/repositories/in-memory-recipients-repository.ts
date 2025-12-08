c327ada028de4b66df4b0a57b2ff1f55d494fe62
BregNights <daniel_baumann567@hotmail.com>
2025-12-08 14:22:27 -0300

    minor update

--- test/repositories/in-memory-recipients-repository.ts
+++ test/repositories/in-memory-recipients-repository.ts
@@ -1,31 +1,21 @@
 import {
-  FindManyNearbyParams,
+  FindManyNearbyCourierParams,
   RecipientsRepository,
 } from "@/domain/carrier/application/repositories/recipients-repository"
 import { getDistanceBetweenCoordinates } from "@/domain/carrier/application/utils/get-distance-between-coordinates"
 import { Recipient } from "@/domain/carrier/enterprise/entities/recipient"
+import { InMemoryCouriersRepository } from "./in-memory-couriers-repository"
+import { InMemoryPackagesRepository } from "./in-memory-packages-repository"
 
 export class InMemoryRecipientsRepository implements RecipientsRepository {
+  constructor(
+    private couriersRepository: InMemoryCouriersRepository,
+    private packagesRepository: InMemoryPackagesRepository
+  ) {}
   public items: Recipient[] = []
 
   async create(recipient: Recipient): Promise<void> {
     this.items.push(recipient)
-  }
-
-  async findManyNearby(params: FindManyNearbyParams) {
-    return this.items.filter((item) => {
-      const distance = getDistanceBetweenCoordinates(
-        { latitude: params.latitude, longitude: params.longitude },
-        {
-          latitude: item.latitude,
-          longitude: item.longitude,
-        }
-      )
-
-      const MAX_DISTANCE_RECIPIENTS_IN_KILOMITERS = 10 //10KM
-
-      return distance < MAX_DISTANCE_RECIPIENTS_IN_KILOMITERS
-    })
   }
 
   async findById(id: string): Promise<Recipient | null> {
@@ -44,6 +34,40 @@ export class InMemoryRecipientsRepository implements RecipientsRepository {
     const recipient = this.items.slice((page - 1) * 20, page * 20)
 
     return recipient
+  }
+
+  async findManyRecipientsNearbyCourier(
+    courierId: string,
+    params: FindManyNearbyCourierParams
+  ): Promise<Recipient[]> {
+    const courier = this.couriersRepository.items.find(
+      (courier) => courier.id.toString() === courierId
+    )
+
+    if (!courier) {
+      throw new Error(`Courier with ID "${courierId}" does not exist.`)
+    }
+
+    const courierPackages = this.packagesRepository.items.filter((pkg) =>
+      pkg.courierId?.equals(courier.id)
+    )
+
+    const recipientsOfCourier = courierPackages
+      .map((pkg) =>
+        this.items.find((recipient) => recipient.id.equals(pkg.recipientId))
+      )
+      .filter(Boolean) as Recipient[]
+
+    return recipientsOfCourier.filter((recipient) => {
+      const distance = getDistanceBetweenCoordinates(
+        { latitude: params.latitude, longitude: params.longitude },
+        { latitude: recipient.latitude, longitude: recipient.longitude }
+      )
+
+      const maxDistanceKm = 50 //50Km
+
+      return distance <= maxDistanceKm
+    })
   }
 
   async save(recipient: Recipient): Promise<void> {
